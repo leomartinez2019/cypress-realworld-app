c6da560aabdf9ca77086ddabcdbe88dca9f582d9
Leonardo Martinez <leonardo.martinez@edunext.co>
2020-08-04 09:11:38 -0500

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const createProxyMiddleware = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/login", "/callback", "/logout", "/checkAuth"], {
-      target: `http://localhost:3001`,
+      target: `http://127.0.0.1:3001`,
       changeOrigin: true,
       logLevel: "debug",
     })
